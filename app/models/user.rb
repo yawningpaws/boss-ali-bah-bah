7113b5db53848abb2c8e6b5014fde7eeffdfa710
Felicia Goh <minhui.felicia@gmail.com>
2021-03-09 17:46:55 +0800

    Send sms notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
-  has_many :notifications, dependent: :destroy
   has_many :payslips, dependent: :destroy
   has_many :accidents, dependent: :destroy
   has_many :workdays, dependent: :destroy
   has_one_attached :ipa_photo
+
+  has_many :notifications, as: :recipient
 end
--- lib/tasks/workday.rake
+++ lib/tasks/workday.rake
@@ -1,7 +1,7 @@
 namespace :workday do
   desc "Send sms to all the workers (todo: who hasn't checked in)"
   task notification: :environment do
-    WorkdayNotification.deliver(User.all)
+    WorkdayNotification.with(message: 'Please check you bloody app').deliver(User.last)
   end
 
 end
